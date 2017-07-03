ffa4ba57a474646009d0c61ecf004a43b243dde4
Xang Xiong <xang.xiong.email@gmail.com>
2017-07-03 10:31:03 -0700

    adding new files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # XIM-JS
 Work in progress project for an Inventory Management system base on Nodejs, Redux, and React.
+
+# Installation
+```
+npm install
+npm start
+```
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+/*
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="/cats" component={CatsPage} />
+  </Route>
+);
+*/
